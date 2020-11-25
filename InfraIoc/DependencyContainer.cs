1612473a385da9de9dbdc815c46ad069eb77d83f
ssrout <smruti.sambit.rout@gmail.com>
2020-11-25 23:35:52 +0530

    initial ioc commit

---
+++ InfraIoc/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core.Bus;
+using InfraBus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace InfraIoc
+{
+    public class DependencyContainer
+    {
+        public static void RegisterService(IServiceCollection services)
+        {
+            //Domian Bus
+            services.AddTransient<IEventBus, RabbitMqBus>();
+        }
+    }
+}
